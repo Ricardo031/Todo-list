48029f72cf3074a41e76af3324b37c88695df322
Ricardo031 <rjtb31@gmail.com>
2024-10-03 16:55:28 -0500

    comentario solo cosas innecesarias y ordenado

--- src/js/main.js
+++ src/js/main.js
@@ -9,22 +9,22 @@ const deleteBtnAll = document.getElementById('eliminar-todo')
 
 addBtn.addEventListener("click", (e) => {
     e.preventDefault(); //para que no se recargue la pag cada vez que se oprima 
-    
+
     const text = input.value;
 
-    if(text !== '') {
+    if (text !== '') {
         const li = document.createElement('li')
-    const p = document.createElement('p')
-    p.textContent = text //aqui ya se agrega el contenido que tiene el texto
+        const p = document.createElement('p')
+        p.textContent = text //aqui ya se agrega el contenido que tiene el texto
 
-    li.appendChild(p)
-    li.appendChild(addModiBtn())
-    li.appendChild(addDeleteBtn())//se va a agregar el boton.
-    ul.appendChild(li)
+        li.appendChild(p)
+        li.appendChild(addModiBtn())
+        li.appendChild(addDeleteBtn()) //se va a agregar el boton.
+        ul.appendChild(li)
 
-    input.value = '';
+        input.value = '';
 
-    empty.style.display = 'none';
+        empty.style.display = 'none';
     }
 
     dele.style.display = 'block' //al momento de oprimir pues que se muestre el contenido
@@ -37,16 +37,16 @@ function addDeleteBtn() {
     deleteBtn.className = 'btn__delete'
 
     deleteBtn.addEventListener('click', (e) => {
-        const item = e.target.parentElement;//nos referimos la boton para poder eliminar al elemento de arriba(padre), no al boton.
+        const item = e.target.parentElement; //nos referimos la boton para poder eliminar al elemento de arriba(padre), no al boton.
         ul.removeChild(item);
 
         const items = document.querySelectorAll('li');
 
-        if(items.length === 0) {
+        if (items.length === 0) {
             empty.style.display = 'block' //para que se vuelva a ver el mensaje
         }
 
-        if(ul.children.length === 0) {
+        if (ul.children.length === 0) {
             dele.style.display = 'none' //para no mostrar la casilla de eliminar 
         }
     })
@@ -57,16 +57,16 @@ deleteBtnAll.addEventListener('click', (e) => {
     e.preventDefault();
 
     //* Eliminar todos los elementos 'li' de la lista
-    while(ul.firstChild) {  //*mientras que ul tenga un primer hijo... entonces
+    while (ul.firstChild) { //*mientras que ul tenga un primer hijo... entonces
         ul.removeChild(ul.firstChild)
-    }//va eliminando al primer hijo y luego pasa al sig hijo hasta que no quede ningun hijo principal. como es un bucle pues es obvio que seguira hasta que no se cumpla dicha condicion
+    } //va eliminando al primer hijo y luego pasa al sig hijo hasta que no quede ningun hijo principal. como es un bucle pues es obvio que seguira hasta que no se cumpla dicha condicion
 
-    if(ul.children.length === 0) {
+    if (ul.children.length === 0) {
         dele.style.display = 'none' //para no mostrar la casilla de eliminar 
     }
 
     const items = document.querySelectorAll('li');
-    if(items.length === 0) {
+    if (items.length === 0) {
         empty.style.display = 'block' //para que se vuelva a ver el mensaje
     }
 })
@@ -79,7 +79,7 @@ function addModiBtn() {
 
     modiBtn.addEventListener('click', (e) => {
         const item = e.target.parentElement;
-        
+
         const newText = prompt("Modifica tu tarea:"); //el prompt para cambiar el contenido
         if (newText !== null && newText.trim() !== '') {
             item.firstChild.textContent = newText; //actualizar li
